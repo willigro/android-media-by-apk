6a128c9fc1004d431a54e3a70f0bb821c89a5d7d
Rittmann <willirittmann@outlook.com>
2023-07-24 11:21:52 -0300

    Creating a new BitmapExif.saveTo that will try to saves the bitmap using the ContentResolver and ContentValues.

--- core/src/main/java/com/rittmann/core/extensions/BitmapExt.kt
+++ core/src/main/java/com/rittmann/core/extensions/BitmapExt.kt
@@ -1,10 +1,16 @@
 package com.rittmann.core.extensions
 
+import android.content.ContentResolver
+import android.content.ContentValues
 import android.graphics.Bitmap
+import android.net.Uri
+import android.provider.MediaStore
+import com.rittmann.core.data.BitmapExif
 import com.rittmann.core.tracker.track
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
+import java.io.OutputStream
 
 
 fun Bitmap.saveTo(file: File): String? {
@@ -25,4 +31,42 @@ fun Bitmap.saveTo(file: File): String? {
         }
     }
     return file.absolutePath
+}
+
+fun BitmapExif?.saveTo(
+    resolver: ContentResolver,
+    contentValues: ContentValues,
+): Uri {
+    var stream: OutputStream? = null
+    var uri: Uri? = null
+
+    return try {
+        val contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        uri = resolver.insert(contentUri, contentValues)
+
+        if (uri == null) {
+            throw IOException("Failed to create new MediaStore record.")
+        }
+
+        stream = resolver.openOutputStream(uri)
+
+        if (stream == null) {
+            throw IOException("Failed to get output stream.")
+        }
+
+        if (this?.bitmap?.compress(Bitmap.CompressFormat.JPEG, 100, stream) == false) {
+            throw IOException("Failed to save bitmap.")
+        }
+
+        uri
+    } catch (e: IOException) {
+        if (uri != null) {
+            // Don't leave an orphan entry in the MediaStore
+            resolver.delete(uri, null, null)
+        }
+
+        throw e
+    } finally {
+        stream?.close()
+    }
 }
